e5351011ef1f25a417805397ac343254eff2b845
jfernandez141 <j-fernandez95@hotmail.com>
2025-07-13 13:13:48 -0500

    Add calculator functions and corresponding tests

---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""
+Sample Tets
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        "Test subtractign number."
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
